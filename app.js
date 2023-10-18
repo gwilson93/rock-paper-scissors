cfa0cfdc636f727e1019fa4fa74446f86b369d6e
Garret Wilson <60952801+gwilson93@users.noreply.github.com>
2023-10-17 22:14:45 -0400

    Implement game logic and winner/loser messaging

--- app.js
+++ app.js
@@ -1,92 +1,124 @@
+// Prevent animation on load
+setTimeout(() => {
+  document.body.classList.remove("preload");
+}, 500);
+
 // DOM
-const btnrules = document.querySelector(".rules-btn")
-const btnclose = document.querySelector(".close-btn")
-const modalRules = document.querySelector(".modal")
+const btnRules = document.querySelector(".rules-btn");
+const btnClose = document.querySelector(".close-btn");
+const modalRules = document.querySelector(".modal");
+
+const CHOICES = [
+  {
+    name: "paper",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    beats: "paper",
+  },
+  {
+    name: "rock",
+    beats: "scissors",
+  },
+];
+const choiceButtons = document.querySelectorAll(".choice-btn");
+const gameDiv = document.querySelector(".game");
+const resultsDiv = document.querySelector(".results");
+const resultDivs = document.querySelectorAll(".results__result");
+
+const resultWinner = document.querySelector(".results__winner");
+const resultText = document.querySelector(".results__text");
+
+const playAgainBtn = document.querySelector(".play-again");
+
+const scoreNumber = document.querySelector(".score__number");
+let score = 0;
+
+// Game Logic
+choiceButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const choiceName = button.dataset.choice;
+    const choice = CHOICES.find((choice) => choice.name === choiceName);
+    choose(choice);
+  });
+});
+
+function choose(choice) {
+  const aichoice = aiChoose();
+  displayResults([choice, aichoice]);
+  displayWinner([choice, aichoice]);
+}
+
+function aiChoose() {
+  const rand = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[rand];
+}
+
+function displayResults(results) {
+  resultDivs.forEach((resultDiv, idx) => {
+    setTimeout(() => {
+      resultDiv.innerHTML = `
+        <div class="choice ${results[idx].name}">
+          <img src="designs/icon-${results[idx].name}.svg" alt="${results[idx].name}" />
+        </div>
+      `;
+    }, idx * 1000);
+  });
+
+  gameDiv.classList.toggle("hidden");
+  resultsDiv.classList.toggle("hidden");
+}
+
+function displayWinner(results) {
+  setTimeout(() => {
+    const userWins = isWinner(results);
+    const aiWins = isWinner(results.reverse());
+
+    if (userWins) {
+      resultText.innerText = "you win";
+      resultDivs[0].classList.toggle("winner");
+      keepScore(1);
+    } else if (aiWins) {
+      resultText.innerText = "you lose";
+      resultDivs[1].classList.toggle("winner");
+      keepScore(-1);
+    } else {
+      resultText.innerText = "draw";
+    }
+    resultWinner.classList.toggle("hidden");
+    resultsDiv.classList.toggle("show-winner");
+  }, 1000);
+}
+
+function isWinner(results) {
+  return results[0].beats === results[1].name;
+}
+
+function keepScore(point) {
+  score += point;
+  scoreNumber.innerText = score;
+}
+
+// Play Again
+playAgainBtn.addEventListener("click", () => {
+  gameDiv.classList.toggle("hidden");
+  resultsDiv.classList.toggle("hidden");
+
+  resultDivs.forEach((resultDiv) => {
+    resultDiv.innerHTML = "";
+    resultDiv.classList.remove("winner");
+  });
+
+  resultText.innerText = "";
+  resultWinner.classList.toggle("hidden");
+  resultsDiv.classList.toggle("show-winner");
+});
 
 // Show/Hide Rules
-btnrules.addEventListener('click', ()=>{
-  modalRules.classList.toggle("show-modal")
-})
-btnclose.addEventListener('click', ()=>{
-  modalRules.classList.toggle("show-modal")
-})
-
-
-function getComputerChoice() {
-  let gameOption = Math.floor(Math.random() * 3);
-  let choice
-  switch (gameOption) {
-    case 0:
-      return choice = "Rock";
-      break;
-    case 1:
-      return choice = "Paper";
-      break;
-    case 2:
-      return choice = "Scissors";
-  }
-}
-
-function playerSelection() {
-  let choice = prompt("Please choose rock, paper, or scissors").toLowerCase();
-  formattedChoice = capitalize(choice)
-  return formattedChoice;
-}
-
-function capitalize(word) {
-  return word[0].toUpperCase() + word.slice(1);
-}
-
-function playRound(playerChoice,computerChoice) {
-  switch (playerChoice + computerChoice) {
-    case "RockPaper":
-    case "ScissorsRock":
-    case "PaperScissors":
-      return "lost";
-      break;
-    case "ScissorsPaper":
-    case "PaperRock":
-    case "RockScissors":
-      return "won";
-      break;
-    default:
-      return "tie";
-  }
-}
-
-function game() {
-  let i = 0
-  let playerScore = 0
-  let computerScore = 0
-  while (i < 5) {
-    let player = playerSelection();
-    let computer = getComputerChoice();
-    let result = playRound(player,computer);
-
-    if (result == "won"){
-      console.log(`You Won! ${player} beats ${computer}.`);
-      playerScore++;
-    }
-    else if (result == "lost"){
-      console.log(`You Lost! ${computer} beats ${player}.`);
-      computerScore++;
-    }
-    else {
-      console.log(`It's a tie! ${player} is the same as ${computer}.`);
-    }
-
-    i++;
-  }
-
-  if (playerScore > computerScore) {
-    console.log(`You Won! The final score is ${playerScore} - ${computerScore}.`);
-  }
-  else if (playerScore < computerScore) {
-    console.log(`You Lost! The final score is ${playerScore} - ${computerScore}.`);
-  }
-  else {
-    console.log(`You Tied! The final score is ${playerScore} - ${computerScore}.`);
-  }
-}
-
-// game()
+btnRules.addEventListener("click", () => {
+  modalRules.classList.toggle("show-modal");
+});
+btnClose.addEventListener("click", () => {
+  modalRules.classList.toggle("show-modal");
+});
